64f4008d77c356b541468d683e83954ee875ee50
Panchowtf <fmiguel_rr@hotmail.com>
2016-10-28 12:49:18 -0500

    the commit of truth

--- tuts/add.cpp
+++ tuts/add.cpp
@@ -1,5 +1,4 @@
 int add(int x, int y)
 {
     return x+y;
-    
 };
--- tuts/main.cpp
+++ tuts/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-#include "add.h"
 
 using namespace std;
-
+int add(int x, int y)
+{
+    return  x+ y;
+}
 int main()
 {
     cout << "the sum of 3+4 is "<< add(3,4)<<endl;
-    return 0;    
+    return 0;
 }
