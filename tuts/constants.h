aaf91df91693c4b2ef94fcf63b4edad8f92ab333
Panchowtf <fmiguel_rr@hotmail.com>
2016-10-29 15:47:07 -0500

    ocommitment

---
+++ tuts/constants.h
@@ -0,0 +1,14 @@
+//
+//  constants.h
+//  tuts
+//
+//  Created by Diseño Spring Air on 29/10/16.
+//  Copyright © 2016 Diseño Spring Air. All rights reserved.
+//
+
+#ifndef constants_h
+#define constants_h
+
+const float gravity{9.8};
+
+#endif /* constants_h */
--- tuts/main.cpp
+++ tuts/main.cpp
@@ -1,26 +1,41 @@
 #include <iostream>
- 
+#include "constants.h"
+using namespace std;
+
+float getInput()
+{
+    cout << "how tall is the tower?";
+    float t;
+    cin >> t;
+    return t;
+}
+
+float compute(float tt, int secs)
+{
+    return tt-(gravity * (secs * secs)/2);
+}
+void printResult(int secs, float result)
+{
+    if (result >= 0)
+        cout << "At" << secs << "seconds the ball is at "<<result<<" meters"<<endl;
+    else
+        cout << "At" << secs << "seconds the ball is at the ground"<<endl;
+}
+
+void calandPrintRes(float tt, int secs)
+{
+    float x = compute(tt,secs);
+    printResult(secs,x);
+}
+
 int main()
 {
-    using namespace std;
-    cout << "bool:\t\t" << sizeof(bool) << " bytes" << endl;
-    cout << "char:\t\t" << sizeof(char) << " bytes" << endl;
-    cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes" << endl;
-    cout << "char16_t:\t" << sizeof(char16_t) << " bytes" << endl; // C++11, may not be supported by your compiler
-    cout << "char32_t:\t" << sizeof(char32_t) << " bytes" << endl; // C++11, may not be supported by your compiler
-    cout << "short:\t\t" << sizeof(short) << " bytes" << endl;
-    cout << "int:\t\t" << sizeof(int) << " bytes" << endl;
-    cout << "long:\t\t" << sizeof(long) << " bytes" << endl;
-    cout << "long long:\t" << sizeof(long long) << " bytes" << endl; // C++11, may not be supported by your compiler
-    cout << "float:\t\t" << sizeof(float) << " bytes" << endl;
-    cout << "double:\t\t" << sizeof(double) << " bytes" << endl;
-    cout << "long double:\t" << sizeof(long double) << " bytes" << endl;
-
-    unsigned short x = 65535; // largest 16-bit unsigned value possible
-    cout << "x was: " << x << endl;
-    x = x + 1; // 65536 is out of our range -- we get overflow because x can't hold 17 bits
-    cout << "x is now: " << x << endl;
-
-    
+    float x = getInput();
+    calandPrintRes(x, 0);
+    calandPrintRes(x, 1);
+    calandPrintRes(x, 2);
+    calandPrintRes(x, 3);
+    calandPrintRes(x, 4);
+    calandPrintRes(x, 5);
     return 0;
 }
