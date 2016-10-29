d1b990ae2d9f1600393e6181125a104b70de6b33
Panchowtf <fmiguel_rr@hotmail.com>
2016-10-29 08:55:10 -0500

    byte size

--- tuts/main.cpp
+++ tuts/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
-
-using namespace std;
-int add(int x, int y)
-{
-    return  x+ y;
-}
+ 
 int main()
 {
-    cout << "the sum of 3+4 is "<< add(3,4)<<endl;
+    using namespace std;
+    cout << "bool:\t\t" << sizeof(bool) << " bytes" << endl;
+    cout << "char:\t\t" << sizeof(char) << " bytes" << endl;
+    cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes" << endl;
+    cout << "char16_t:\t" << sizeof(char16_t) << " bytes" << endl; // C++11, may not be supported by your compiler
+    cout << "char32_t:\t" << sizeof(char32_t) << " bytes" << endl; // C++11, may not be supported by your compiler
+    cout << "short:\t\t" << sizeof(short) << " bytes" << endl;
+    cout << "int:\t\t" << sizeof(int) << " bytes" << endl;
+    cout << "long:\t\t" << sizeof(long) << " bytes" << endl;
+    cout << "long long:\t" << sizeof(long long) << " bytes" << endl; // C++11, may not be supported by your compiler
+    cout << "float:\t\t" << sizeof(float) << " bytes" << endl;
+    cout << "double:\t\t" << sizeof(double) << " bytes" << endl;
+    cout << "long double:\t" << sizeof(long double) << " bytes" << endl;
+
+    unsigned short x = 65535; // largest 16-bit unsigned value possible
+    cout << "x was: " << x << endl;
+    x = x + 1; // 65536 is out of our range -- we get overflow because x can't hold 17 bits
+    cout << "x is now: " << x << endl;
+
+    
     return 0;
 }
