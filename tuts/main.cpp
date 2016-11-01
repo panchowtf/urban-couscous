b593614e03407c28bfcbfcaaae043e98a03b6797
Panchowtf <fmiguel_rr@hotmail.com>
2016-11-01 13:29:28 -0600

    message

--- tuts/main.cpp
+++ tuts/main.cpp
@@ -4,38 +4,24 @@ using namespace std;
 
 float getInput()
 {
-    cout << "how tall is the tower?";
+    cout << "Pick a number :";
     float t;
     cin >> t;
     return t;
 }
 
-float compute(float tt, int secs)
+void compute(int t)
 {
-    return tt-(gravity * (secs * secs)/2);
-}
-void printResult(int secs, float result)
-{
-    if (result >= 0)
-        cout << "At" << secs << "seconds the ball is at "<<result<<" meters"<<endl;
+    if (t % 2==0)
+        cout << "the number is even"<<  endl;
     else
-        cout << "At" << secs << "seconds the ball is at the ground"<<endl;
-}
-
-void calandPrintRes(float tt, int secs)
-{
-    float x = compute(tt,secs);
-    printResult(secs,x);
+        cout << "the number is odd" << endl;
 }
 
 int main()
 {
-    float x = getInput();
-    calandPrintRes(x, 0);
-    calandPrintRes(x, 1);
-    calandPrintRes(x, 2);
-    calandPrintRes(x, 3);
-    calandPrintRes(x, 4);
-    calandPrintRes(x, 5);
+    int n;
+    n = getInput();
+    compute(n);
     return 0;
 }
